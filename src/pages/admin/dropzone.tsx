8c01c64105b296be6038a95225c0aa3936f379ce
Aliaksandr Matveichykau <kobuqa@gmail.com>
2024-01-16 18:50:28 +0300

    feat: admin dnd POC

---
+++ src/pages/admin/dropzone.tsx
@@ -0,0 +1,33 @@
+import { cn } from "@/shared/lib/utils";
+import { useCallback } from "react";
+import { useDropzone } from "react-dropzone";
+
+type Props = {
+  onFileAdd: (file: File[]) => void;
+};
+export const DropZone = ({ onFileAdd }: Props) => {
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    onFileAdd(acceptedFiles);
+    console.log(acceptedFiles);
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  return (
+    <div
+      {...getRootProps()}
+      className={cn(
+        "0 border border-dashed rounded-md p-4 w-full h-64 flex items-center justify-center",
+        {
+          "bg-blue-700/50": isDragActive,
+        }
+      )}
+    >
+      <input {...getInputProps()} />
+      {isDragActive ? (
+        <p>Drop the files here ...</p>
+      ) : (
+        <p>Drag 'n' drop some files here, or click to select files</p>
+      )}
+    </div>
+  );
+};
--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,3 +1,34 @@
+import { useState } from "react";
+import { DropZone } from "./dropzone";
+import { Button } from "@/shared/ui/button";
+import { X } from "lucide-react";
+
 export const AdminPage = () => {
-  return <div>AdminPage</div>;
+  const [files, setFiles] = useState<File[]>([]);
+
+  const addFile = (file: File[]) => setFiles((p) => [...p, ...file]);
+  const removeFile = (fileName: string) =>
+    setFiles((p) => p.filter((file) => file.name !== fileName));
+
+  return (
+    <div className="w-full h-full flex flex-col items-center gap-6 py-8 max-w-3xl mx-auto">
+      <DropZone onFileAdd={addFile} />
+      <ul className="w-full">
+        {files.length === 0 && <li>No files</li>}
+        {files.map((file) => (
+          <li key={file.size} className="flex items-center gap-4">
+            {file.name}
+            <button
+              className="hover:text-red-500 transition-colors"
+              onClick={() => removeFile(file.name)}
+            >
+              <X />
+            </button>
+          </li>
+        ))}
+      </ul>
+
+      <Button disabled>Upload</Button>
+    </div>
+  );
 };
